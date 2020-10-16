e4752b0ebfed5cb12e0b43f6cb2b66128d049367
Aldhaneka <67791514+Aldhanekaa@users.noreply.github.com>
2020-10-16 21:33:11 +0700

    Create Spinal Tap Case.js

---
+++ Intermediate Algorithm Scripting/Spinal Tap Case.js
@@ -0,0 +1,22 @@
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+function spinalCase(str) {
+  return str
+  .replace(/([A-Z])/g, ' $1')
+  .toLowerCase()
+  .trim()
+  .split(/[\W_]/)
+  .filter(e => e!== '').join('-')
+  ;
+}
+
+console.log(spinalCase('AllThe-small Things'));
+
+// Hello Wrold -> hello-world
+// convert to the lower case
+// we can split it then we can use join method
+
+
+
+
+
