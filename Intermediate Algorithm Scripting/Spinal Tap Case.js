a94353c688c8d8a60ce9a9c54b89d2bec4872551
Aldhaneka <67791514+Aldhanekaa@users.noreply.github.com>
2020-10-17 15:45:09 +0700

    Update Spinal Tap Case.js

--- Intermediate Algorithm Scripting/Spinal Tap Case.js
+++ Intermediate Algorithm Scripting/Spinal Tap Case.js
@@ -1,4 +1,4 @@
-Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 
 function spinalCase(str) {
   return str
