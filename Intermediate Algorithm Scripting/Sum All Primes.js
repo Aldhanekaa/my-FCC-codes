90f9353acb33b625dac6c3f985c67a4cbb955ba8
Aldhaneka <67791514+Aldhanekaa@users.noreply.github.com>
2020-10-17 08:38:52 +0700

    Create Sum All Primes.js

---
+++ Intermediate Algorithm Scripting/Sum All Primes.js
@@ -0,0 +1,20 @@
+function sumPrimes(num) {
+  return getPrimes(num).reduce((prev, current) => prev+current);
+}
+
+console.log(sumPrimes(10));
+
+function getPrimes(max) {
+    var sieve = [], i, j, primes = [];
+    for (i = 2; i <= max; ++i) {
+        if (!sieve[i]) {
+            // i has not been marked -- it is prime
+            primes.push(i);
+            for (j = i << 1; j <= max; j += i) {
+                sieve[j] = true;
+            }
+        }
+    }
+    return primes;
+}
+// console.log(getPrimes(10));
